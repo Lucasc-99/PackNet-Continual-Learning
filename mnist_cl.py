34595638def6937e2135b597637869f4528247ac
Lucas Cecchi <lucascecchi@gmail.com>
2021-06-08 17:30:07 -0400

    Implemented continual training on MNIST, Fashion-MNIST, and KMNIST in mnist_cl.py

---
+++ mnist_cl.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from tqdm import tqdm
+from net import MnistClassifier
+
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,)),
+                                ])
+
+#
+# Datasets
+#
+print("Downloading Datasets")
+trainloaders = []
+testloaders = []
+
+# MNIST
+train = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+test = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+trainloaders.append(torch.utils.data.DataLoader(train, batch_size=64, shuffle=True))
+testloaders.append(torch.utils.data.DataLoader(test, batch_size=1, shuffle=True))
+
+# FashionMNIST
+#train = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+#test = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
+#trainloaders.append(torch.utils.data.DataLoader(train, batch_size=64, shuffle=True))
+#testloaders.append(torch.utils.data.DataLoader(test, batch_size=1, shuffle=True))
+
+# KMNIST
+#train = datasets.KMNIST(root='./data', train=True, download=True, transform=transform)
+#test = datasets.KMNIST(root='./data', train=False, download=True, transform=transform)
+#trainloaders.append(torch.utils.data.DataLoader(train, batch_size=64, shuffle=True))
+#testloaders.append(torch.utils.data.DataLoader(test, batch_size=1, shuffle=True))
+
+
+#
+# Define model and hyperparameters
+#
+model = MnistClassifier()
+LR = .01
+N_EPOCH = 5
+loss = nn.NLLLoss()
+sgd_optim = optim.SGD(model.parameters(), lr=LR)
+
+
+#
+# Training loop
+#
+print("Training Model")
+for loader in trainloaders:
+    sgd_optim = optim.SGD(model.parameters(), lr=LR)  # Recreate optimizer on task switch
+    for img, cl in tqdm(loader):
+        model.zero_grad()
+        l = loss(model(img), cl)
+        l.backward()
+        sgd_optim.step()
+
+#
+# Test Loop
+#
+accuracy = []
+print("\nTesting Model")
+for loader in testloaders:
+    t1 = 0
+    for img, cl in tqdm(loader):
+        pred = torch.argmax(model(img)[0])
+        if pred != cl[0]:
+            t1 += 1
+    accuracy.append(1.0 - (t1 / len(loader)))
+
+
+# Results
+print("")
+for i, r in enumerate(accuracy):
+    print(f'Accuracy on task {i + 1} : {r}')
--- net.py
+++ net.py
@@ -5,10 +5,10 @@ import torch.nn.functional as F
 
 class MnistClassifier(nn.Module):
 
-    def __init__(self):
+    def __init__(self, input_channels=1):
         super(MnistClassifier, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5)
+        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=10, kernel_size=5)
         self.dense1 = nn.Linear(in_features=5760, out_features=2000)
         self.dense2 = nn.Linear(in_features=2000, out_features=10)
 
@@ -18,7 +18,3 @@ class MnistClassifier(nn.Module):
         x = F.relu(self.dense1(x))
         x = F.relu(self.dense2(x))
         return F.log_softmax(x, dim=1)
-
-
-
-
