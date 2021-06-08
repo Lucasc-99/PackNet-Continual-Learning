0ba41beb5e71e3862860d999416360e05f074031
Lucas Cecchi <lucascecchi@gmail.com>
2021-06-08 17:31:33 -0400

    Uncommented datasets

--- mnist_cl.py
+++ mnist_cl.py
@@ -24,16 +24,16 @@ trainloaders.append(torch.utils.data.DataLoader(train, batch_size=64, shuffle=Tr
 testloaders.append(torch.utils.data.DataLoader(test, batch_size=1, shuffle=True))
 
 # FashionMNIST
-#train = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
-#test = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
-#trainloaders.append(torch.utils.data.DataLoader(train, batch_size=64, shuffle=True))
-#testloaders.append(torch.utils.data.DataLoader(test, batch_size=1, shuffle=True))
+train = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+test = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
+trainloaders.append(torch.utils.data.DataLoader(train, batch_size=64, shuffle=True))
+testloaders.append(torch.utils.data.DataLoader(test, batch_size=1, shuffle=True))
 
 # KMNIST
-#train = datasets.KMNIST(root='./data', train=True, download=True, transform=transform)
-#test = datasets.KMNIST(root='./data', train=False, download=True, transform=transform)
-#trainloaders.append(torch.utils.data.DataLoader(train, batch_size=64, shuffle=True))
-#testloaders.append(torch.utils.data.DataLoader(test, batch_size=1, shuffle=True))
+train = datasets.KMNIST(root='./data', train=True, download=True, transform=transform)
+test = datasets.KMNIST(root='./data', train=False, download=True, transform=transform)
+trainloaders.append(torch.utils.data.DataLoader(train, batch_size=64, shuffle=True))
+testloaders.append(torch.utils.data.DataLoader(test, batch_size=1, shuffle=True))
 
 
 #
